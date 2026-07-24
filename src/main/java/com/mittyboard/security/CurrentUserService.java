26cc9b51a430507b5af259271ef44f1b38bf9f61
Atakan Ünsever <atakanunsever.04@gmail.com>
2026-07-25 00:58:25 +0300

    feat: Security feature done.

---
+++ src/main/java/com/mittyboard/security/CurrentUserService.java
@@ -0,0 +1,20 @@
+package com.mittyboard.security;
+
+import com.mittyboard.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CurrentUserService {
+
+    public User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated() || authentication.getPrincipal().equals("anonymousUser")) {
+            return null;
+        }
+
+        return (User) authentication.getPrincipal();
+    }
+}
