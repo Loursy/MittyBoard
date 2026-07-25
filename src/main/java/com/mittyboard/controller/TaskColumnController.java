682d26f57dd2eefae1fff86b378c6306de942f61
Atakan Ünsever <atakanunsever.04@gmail.com>
2026-07-25 20:49:55 +0300

    feat: TaskColumn added.

---
+++ src/main/java/com/mittyboard/controller/TaskColumnController.java
@@ -0,0 +1,31 @@
+package com.mittyboard.controller;
+
+import com.mittyboard.dto.TaskColumnRequest;
+import com.mittyboard.dto.TaskColumnResponse;
+import com.mittyboard.service.TaskColumnService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/columns")
+@RequiredArgsConstructor
+public class TaskColumnController {
+
+    private final TaskColumnService taskColumnService;
+
+    @PostMapping
+    public ResponseEntity<TaskColumnResponse> createColumn(@RequestBody TaskColumnRequest request) {
+        TaskColumnResponse response = taskColumnService.createColumn(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskColumnResponse>> getColumns(@RequestParam Long boardId) {
+        List<TaskColumnResponse> responses = taskColumnService.getColumnsByBoard(boardId);
+        return ResponseEntity.ok(responses);
+    }
+}
