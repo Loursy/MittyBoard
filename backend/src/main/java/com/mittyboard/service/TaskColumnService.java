d407a7fe8bf4a43038640df9d6005e7a6dacd8d5
Atakan Ünsever <atakanunsever.04@gmail.com>
2026-07-30 17:13:59 +0300

    feat: CRUD operations added to Task Column

--- backend/src/main/java/com/mittyboard/service/TaskColumnService.java
+++ backend/src/main/java/com/mittyboard/service/TaskColumnService.java
@@ -60,6 +60,36 @@ public class TaskColumnService {
                 .collect(Collectors.toList());
     }
 
+    public TaskColumnResponse updateTaskColumn(Long taskcolumnId, TaskColumnRequest request) {
+        User currentUser = currentUserService.getAuthenticatedUser();
+
+        TaskColumn taskColumn = taskColumnRepository.findById(taskcolumnId)
+                .orElseThrow(() -> new RuntimeException("Task Column cannot be found."));
+
+        if(!taskColumn.getBoard().getWorkspace().getOwner().getId().equals(currentUser.getId())) {
+            throw new RuntimeException("You don't have access to update the column in this Task Column");
+        }
+
+        taskColumn.setTitle(request.getTitle());
+        taskColumn.setPosition(request.getPosition());
+
+        TaskColumn updatedColumn = taskColumnRepository.save(taskColumn);
+
+        return mapToResponse(updatedColumn);
+    }
+
+    public void deleteTaskColumn(Long taskcolumnId) {
+        User currentUser = currentUserService.getAuthenticatedUser();
+
+        TaskColumn taskColumn = taskColumnRepository.findById(taskcolumnId)
+                .orElseThrow(() -> new RuntimeException("Task Column cannot be found!"));
+
+        if(!taskColumn.getBoard().getWorkspace().getOwner().getId().equals(currentUser.getId())) {
+            throw new RuntimeException("You are not authorized to delete this Task Column!");
+        }
+
+        taskColumnRepository.delete(taskColumn);
+    }
 
     private TaskColumnResponse mapToResponse(TaskColumn column) {
         return TaskColumnResponse.builder()
