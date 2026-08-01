fee808ebca8f7c6f1eda39e43001762a8c4c2fa4
Atakan Ünsever <atakanunsever.04@gmail.com>
2026-08-01 17:39:43 +0300

    feat: BoardServiceTest added

--- backend/src/main/java/com/mittyboard/controller/BoardController.java
+++ backend/src/main/java/com/mittyboard/controller/BoardController.java
@@ -7,7 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 import java.util.List;
 
@@ -18,31 +17,36 @@ public class BoardController {
 
     private final BoardService boardService;
 
-    @PostMapping
-    public ResponseEntity<BoardResponse> createBoard(@RequestBody BoardRequest request) {
-        BoardResponse response = boardService.createBoard(request);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    @PostMapping("/workspaces/{workspaceId}")
+    public ResponseEntity<BoardResponse> createBoard(
+            @PathVariable Long workspaceId,
+            @RequestBody BoardRequest request) {
+
+        BoardResponse response = boardService.createBoard(workspaceId, request);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
-    @GetMapping
-    public ResponseEntity<List<BoardResponse>> getBoards(@RequestParam Long workspaceId) {
+    @GetMapping("/workspaces/{workspaceId}")
+    public ResponseEntity<List<BoardResponse>> getBoards(@PathVariable Long workspaceId) {
+
         List<BoardResponse> responses = boardService.getBoardsByWorkspace(workspaceId);
         return ResponseEntity.ok(responses);
     }
 
-    @PutMapping
+    @PutMapping("/{id}")
     public ResponseEntity<BoardResponse> updateBoard(
             @PathVariable Long id,
-            @RequestBody BoardRequest request
-    ) {
-        BoardResponse response = boardService.updateBoard(id , request);
+            @RequestBody BoardRequest request) {
+
+        BoardResponse response = boardService.updateBoard(id, request);
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteBoard(@PathVariable Long id) {
+
         boardService.deleteBoard(id);
         return ResponseEntity.noContent().build();
     }
-
 }
